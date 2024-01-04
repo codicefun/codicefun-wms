995bfb6b2ae1d8407679f1ae182969b754de158e
James Wang <james_wangx@163.com>
2024-01-04 14:37:50 +0800

    Add create user

--- codicefun-wms-api/src/main/java/com/codicefun/wms/entity/po/User.java
+++ codicefun-wms-api/src/main/java/com/codicefun/wms/entity/po/User.java
@@ -1,10 +1,13 @@
 package com.codicefun.wms.entity.po;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class User {
 
+    @TableId(type = IdType.AUTO)
     private Integer id;
 
     private String username;
--- codicefun-wms-app/src/views/user.vue
+++ codicefun-wms-app/src/views/user.vue
@@ -1,25 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { reqChangeState, reqList, reqUpdateById } from '@/request/user'
+import { reqChangeState, reqCreate, reqList, reqUpdateById } from '@/request/user'
 import { ElMessage } from 'element-plus'
 import type { User } from '@/request/user/type'
 
 const tableData = ref<User[]>()
 const formData = ref<User>({} as User)
+const formTitle = ref<string>()
+
 const current = ref<number>()
 const size = ref<number>()
 const total = ref<number>()
-const small = ref(false)
-const background = ref(false)
-const disabled = ref(false)
 const dialogVisible = ref(false)
 
-const getData = async () => {
+const updateData = async () => {
   const resp = await reqList()
 
   if (resp.code === 200) {
-    current.value = resp.data.current
-    size.value = resp.data.size
     total.value = resp.data.total
     tableData.value = resp.data.list
   }
@@ -46,6 +43,7 @@ const handleSizeChange = async (val: number) => {
 const showEditDialog = (row: User) => {
   dialogVisible.value = true
   formData.value = { ...row }
+  formTitle.value = '修改用户信息'
 }
 
 const edit = async () => {
@@ -54,7 +52,7 @@ const edit = async () => {
 
   if (resp.code === 200) {
     // Update table data
-    await getData()
+    await updateData()
     ElMessage({
       showClose: true,
       message: '修改成功',
@@ -68,7 +66,7 @@ const changeState = async (id: number) => {
 
   if (resp.code === 200) {
     // Update table data
-    await getData()
+    await updateData()
     ElMessage({
       showClose: true,
       message: '修改成功',
@@ -77,13 +75,46 @@ const changeState = async (id: number) => {
   }
 }
 
+const showCreateDialog = () => {
+  dialogVisible.value = true
+  formData.value = {} as User
+  formTitle.value = '新增用户信息'
+}
+
+const create = async () => {
+  dialogVisible.value = false
+  const resp = await reqCreate(formData.value)
+
+  if (resp.code === 200) {
+    // Update table data
+    await updateData()
+    ElMessage({
+      showClose: true,
+      message: '新增成功',
+      type: 'success'
+    })
+  }
+}
+
 // Get data in the beginning
-getData()
+reqList().then(resp => {
+  if (resp.code === 200) {
+    current.value = resp.data.current
+    size.value = resp.data.size
+    total.value = resp.data.total
+    tableData.value = resp.data.list
+  }
+})
 </script>
 
 <template>
+  <el-row>
+    <el-col :span="24">
+      <el-button type="primary" @click="showCreateDialog">新增</el-button>
+    </el-col>
+  </el-row>
   <el-scrollbar>
-    <el-table size="large" :data="tableData" height="75vh" style="width: 100%;">
+    <el-table size="large" :data="tableData" style="width: 100%;">
       <el-table-column prop="id" label="ID" width="100" />
       <el-table-column prop="username" label="用户名" width="100" />
       <el-table-column prop="nickname" label="昵称" width="100" />
@@ -124,9 +155,6 @@ getData()
       :current-page="current"
       :page-size="size"
       :page-sizes="[5, 10, 20]"
-      :small="small"
-      :disabled="disabled"
-      :background="background"
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
       @current-change="handleCurrentChange"
@@ -135,7 +163,7 @@ getData()
   </div>
   <el-dialog
     v-model="dialogVisible"
-    title="修改用户信息"
+    :title="formTitle"
     width="30%"
   >
     <el-form
@@ -144,6 +172,9 @@ getData()
       style="max-width: 460px">
       <el-form-item label="用户名">
         <el-input v-model="formData.username" />
+      </el-form-item>
+      <el-form-item v-if="formTitle?.startsWith('新增')" label="密码">
+        <el-input type="password" v-model="formData.password" />
       </el-form-item>
       <el-form-item label="昵称">
         <el-input v-model="formData.nickname" />
@@ -177,9 +208,8 @@ getData()
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="edit">
-          确认
-        </el-button>
+        <el-button v-if="formTitle?.startsWith('修改')" type="primary" @click="edit">确认</el-button>
+        <el-button v-else type="primary" @click="create">确认</el-button>
       </span>
     </template>
   </el-dialog>
@@ -187,7 +217,7 @@ getData()
 
 <style scoped>
 .el-scrollbar {
-  height: auto;
+  height: 70vh;
 }
 
 .demo-pagination-block {
